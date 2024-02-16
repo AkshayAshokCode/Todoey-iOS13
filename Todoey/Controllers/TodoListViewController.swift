0334f4de59f68c2856854365fbad5a797b25525b
akshay ashok <akshayashokan1054@gmail.com>
2024-02-16 05:53:54 +0530

    add didTextChange in UISearchBar

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -111,9 +111,9 @@ class TodoListViewController: UITableViewController {
         }catch{
            print("Error fetching data from context \(error)")
         }
+        
+        self.tableView.reloadData()
     }
-    
- 
 }
 
 //MARK: - Search bar methods
@@ -128,6 +128,16 @@ extension TodoListViewController: UISearchBarDelegate{
         request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
         
         loadItems(with: request)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if(searchBar.text?.count == 0){
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
     }
     
 }
