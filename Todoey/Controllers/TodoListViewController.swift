6daba9457a62d131f70ce45ac79df17057fee616
akshay ashok <akshayashokan1054@gmail.com>
2024-02-26 08:01:34 +0530

    Add super class of SwipeTableViewController

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import RealmSwift
 
-class TodoListViewController: UITableViewController {
+class TodoListViewController: SwipeTableViewController {
     
     var todoItems :Results<Item>?
     let realm = try! Realm()
@@ -45,7 +45,7 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
         
         if todoItems?.count == 0{
             cell.textLabel?.text = "No Items Added"
@@ -58,15 +58,15 @@ class TodoListViewController: UITableViewController {
             }
         }
         
-        if let item = todoItems?[indexPath.row] {
-            cell.textLabel?.text = item.title
-            
-            cell.accessoryType = item.done ? .checkmark : .none
-            
-        }else{
-            cell.textLabel?.text = "No Items Added"
-        }
-        
+//        if let item = todoItems?[indexPath.row] {
+//            cell.textLabel?.text = item.title
+//            
+//            cell.accessoryType = item.done ? .checkmark : .none
+//            
+//        }else{
+//            cell.textLabel?.text = "No Items Added"
+//        }
+//        
        
         return cell
     }
@@ -89,6 +89,21 @@ class TodoListViewController: UITableViewController {
        
         tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //MARK: Delete Data from Swipe
+    override func updateModel(at indexPath: IndexPath) {
+        if let itemForDeletion = self.todoItems?[indexPath.row] {
+            do{
+                try self.realm.write{
+                    self.realm.delete(itemForDeletion)
+                }
+            }catch{
+               print("Error saving context \(error)")
+            }
+            
+        }
+     
     }
     
     //MARK: Add New Items
